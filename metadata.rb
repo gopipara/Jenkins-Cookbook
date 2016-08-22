d949ad41ff7ae922703eb1ce04200d85aa635b5e
gopipara <gopi.para1@marist.edu>
2016-08-21 17:06:26 -0700

    Jenkins Cookbook

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name             'jenkins'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache 2.0'
+description      'Installs and configures Jenkins CI master & slaves'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '2.6.0'
+
+source_url 'https://github.com/chef-cookbooks/jenkins'
+issues_url 'https://github.com/chef-cookbooks/jenkins/issues'
+
+recipe 'jenkins::master', 'Installs a Jenkins master'
+
+depends 'apt',   '>= 2.0'
+depends 'runit', '~> 1.7'
+depends 'yum',   '~> 3.0'
